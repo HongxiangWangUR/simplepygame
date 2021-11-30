aac31c22d3c91b16d47f6fae5f5aa98bbf2f99c5
HongxiangWangUR <whx0509@gmail.com>
2021-12-01 00:19:35 +0800

    add floor img

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ SCREEN=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("my game")
 clock=pygame.time.Clock()
 SCREEN.fill(COLOR_BLACK)
-backgroundpic=pygame.image.load('./pics/background.png').convert()
+backgroundpic=pygame.image.load('./pics/bg.png').convert()
+backgroundpic=pygame.transform.scale2x(backgroundpic)
+floorpic=pygame.image.load('./pics/floor.png').convert()
+floorpic=pygame.transform.scale(floorpic,(800,100))
 
 SCREEN.blit(backgroundpic,(0,0))
+SCREEN.blit(floorpic,(0,700))
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.locals.QUIT:
