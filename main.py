7b6d4671d3eac097e4a697c4122fe0be3e12e3bd
HongxiangWangUR <whx0509@gmail.com>
2021-12-05 22:06:26 +0800

    basic functions finished"

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pygame,sys
 import pygame.locals
 from gameconstant import *
+import random
 
 pygame.init()
 
@@ -15,10 +16,10 @@ backgroundpic=pygame.image.load('./pics/bg.png').convert()
 backgroundpic=pygame.transform.scale2x(backgroundpic)
 floorpic=pygame.image.load('./pics/floor.png').convert()
 floorpic=pygame.transform.scale(floorpic,(1000,100))
-birdpic=pygame.image.load('./pics/bird.png').convert_alpha()
+birdpic=pygame.image.load('./pics/bird1.png').convert_alpha()
 birdpic=pygame.transform.scale(birdpic,(56,44))
 pipe_surface=pygame.image.load("./pics/pipe.gif").convert()
-pipe_surface=pygame.transform.scale(pipe_surface,(70,400))
+pipe_surface=pygame.transform.scale(pipe_surface,(70,500))
 
 
 SCREEN.blit(backgroundpic,(0,0))
@@ -38,6 +39,10 @@ pipe_list=[]
 
 ADD_PIPE=pygame.USEREVENT+1
 pygame.time.set_timer(ADD_PIPE,1000)
+
+game_active=True
+
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.locals.QUIT:
@@ -48,26 +53,43 @@ while True:
 				birdrect.y-=50
 				drop_speed=0
 		if event.type == ADD_PIPE:
-			pipe_rect=pipe_surface.get_rect()
-			pipe_rect.x=500
-			pipe_rect.y=500
-			pipe_list.append(pipe_rect)
+			if game_active:
+				pipe_rect=pipe_surface.get_rect()
+				pipe_rect.x=500
+				pipe_rect.y=random.randint(400,650)
 
+				top_pipe_rect=pipe_surface.get_rect()
+				top_pipe_rect.bottomleft=(500,pipe_rect.y-200)
+				pipe_list.append((pipe_rect,top_pipe_rect))
+			
+
+	SCREEN.blit(backgroundpic,(0,0))
 	floorrect.move_ip(-SPEED,0)
+
+	if game_active:
+		for pipes in pipe_list:
+			for pipe in pipes:
+				pipe.move_ip(-SPEED,0)
+
 	if floorrect.x < -500:
 		floorrect.x=0
 	birdrect.move_ip(0,drop_speed)
-	SCREEN.blit(backgroundpic,(0,0))
 
-	for pipe in pipe_list:
-		SCREEN.blit(pipe_surface,pipe)
-
-	SCREEN.blit(birdpic,birdrect)
+	if game_active:
+		for pipe in pipe_list:
+			SCREEN.blit(pipe_surface,pipe[0])
+			SCREEN.blit(pygame.transform.flip(pipe_surface,False,True),pipe[1])
+	if game_active:
+		SCREEN.blit(birdpic,birdrect)
 	SCREEN.blit(floorpic,floorrect)
 
 	pygame.display.update()
+
+	for pipes in pipe_list:
+		for pipe in pipes:
+			if pipe.colliderect(birdrect):
+				pipe_list.clear()
+				game_active=False
 	drop_speed+=drop_acceleration
-	for pipe in pipe_list:
-		pipe.move_ip(-SPEED,0)
 	clock.tick(60)
 
